28ba2c0ffa3642c128fd16e0210bf1173edfbbaf
Devan <devan23.5.2001@gmail.com>
2020-11-27 10:24:44 -0500

    added primitives.rs

---
+++ primitives.rs
@@ -0,0 +1,21 @@
+fn main()   {
+let logical:bool=true;
+
+let a_float:f64=42.0;
+let an_integer= 999i32;  //suffix annotation
+
+//default for float is f64
+//default for int is i32
+
+let mut inferred_type=1232; //type i is inferred
+
+inferred_type=1234; //mutable values can be re-assigned
+
+let immutable_value=123;
+let immutable_value=11;//overriting immutable values with shadowing
+
+
+
+
+
+}
