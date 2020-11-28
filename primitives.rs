ad7633ad3a618fdf404a705817eb6996df122294
Devan <devan23.5.2001@gmail.com>
2020-11-27 21:04:12 -0500

    modified primitives.rs

--- primitives.rs
+++ primitives.rs
@@ -1,21 +1,26 @@
-fn main()   {
-let logical:bool=true;
+fn main() {
+    // Variables can be type annotated.
+    let logical: bool = true;
 
-let a_float:f64=42.0;
-let an_integer= 999i32;  //suffix annotation
+    let a_float: f64 = 1.0;  // Regular annotation
+    let an_integer   = 5i32; // Suffix annotation
 
-//default for float is f64
-//default for int is i32
-
-let mut inferred_type=1232; //type i is inferred
-
-inferred_type=1234; //mutable values can be re-assigned
-
-let immutable_value=123;
-let immutable_value=11;//overriting immutable values with shadowing
-
-
-
-
-
+    // Or a default will be used.
+    let default_float   = 3.0; // `f64`
+    let default_integer = 7;   // `i32`
+    
+    // A type can also be inferred from context 
+    let mut inferred_type = 12; // Type i64 is inferred from another line
+    inferred_type = 4294967296i64;
+    
+    // A mutable variable's value can be changed.
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+    
+    // Error! The type of a variable can't be changed.
+   // mutable = true;
+    
+    // Variables can be overwritten with shadowing.
+    let mutable = true;
 }
+
